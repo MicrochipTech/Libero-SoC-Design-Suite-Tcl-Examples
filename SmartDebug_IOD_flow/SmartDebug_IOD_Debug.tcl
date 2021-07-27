60366b89dd91a2a731266bc5827d8459db39b314
Kiran Palla <Kiran.Palla@microchip.com>
2021-07-27 16:14:05 -0700

    updated

--- SmartDebug_IOD_flow/SmartDebug_IOD_Debug.tcl
+++ SmartDebug_IOD_flow/SmartDebug_IOD_Debug.tcl
@@ -1,3 +1,8 @@
+#-------------------------------------------------------------------------------
+# 
+# Description: This script runs debug_iod command on the IOD design
+#--------------------------------------------------------------------
+
 
 set IOD_Training_Data [ debug_iod -iod_type {RX_DDRX_B_G_DYN} -inst_path {PF_IOD_GENERIC_RX_C1_0} ]
 
